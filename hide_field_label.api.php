48f99812672c69cf80e881e89a236e4810ca596b
Serge Skripchuk <s.skripchuk@internetdevels.com>
2016-04-12 11:00:10 +0300

    Fixed coding standards

--- hide_field_label.api.php
+++ hide_field_label.api.php
@@ -7,9 +7,6 @@
 
 /**
  * Alters Hide Field Label supported widgets.
- *
- * @return array
- *   An array of supported widgets and their value fields.
  */
 function hook_hide_field_label_widgets_alter(&$widgets) {
   $widgets['phone_textfield'] = 'value';
